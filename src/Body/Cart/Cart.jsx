fc6acad35bc60f6c8db96b9679cacfd1b9b48098
Muhammed Ansas pt <muhammedansas566@gmail.com>
2024-08-28 11:22:03 +0530

    updated

--- src/Body/Cart/Cart.jsx
+++ src/Body/Cart/Cart.jsx
@@ -4,11 +4,16 @@ import "./Cart.css"
 import {
   MDBCard,
   MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
+  MDBCardFooter,
+  MDBCardHeader,
   MDBCardImage,
-  MDBBtn
-} from 'mdb-react-ui-kit';
+  MDBCol,
+  MDBContainer,
+  MDBProgress,
+  MDBProgressBar,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 
 function Cart() {
   const {user} = useContext(Addcontext)
@@ -32,18 +37,250 @@ user.cart = cartData
 setRerender(!reRender)
   }
 console.log(user.cart.length);
+console.log(user);
+
  
   return (
-    <div style={{backgroundColor:'green'}}>
-     
-        <h1>Cart is Empty Please Add cart</h1>
-      <>
-      <span className='cart-total' style={{width:"400px",height:"100vh",backgroundColor:"black",position:"sticky",marginLeft:"810px"}}>
-      <h1>Total price</h1>
-      <h1>Demo price</h1>
-    </span>
+    <>
+    <section
+        className="h-100 gradient-custom"
+        style={{ backgroundColor: "#eee" }}
+      >
+        <MDBContainer className="py-5 h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol lg="10" xl="8">
+              <MDBCard style={{ borderRadius: "10px" }}>
+                <MDBCardHeader className="px-4 py-5">
+                  <MDBTypography tag="h5" className="text-muted mb-0">
+                    Thanks for your Order,{" "}
+                    <span style={{ color: "#a8729a" }}>Anna</span>!
+                  </MDBTypography>
+                </MDBCardHeader>
+                <MDBCardBody className="p-4">
+                  <div className="d-flex justify-content-between align-items-center mb-4">
+                    <p
+                      className="lead fw-normal mb-0"
+                      style={{ color: "#a8729a" }}
+                    >
+                      Receipt
+                    </p>
+                    <p className="small text-muted mb-0">
+                      Receipt Voucher : 1KAU9-84UIL
+                    </p>
+                  </div>
+
+                  <MDBCard className="shadow-0 border mb-4">
+                    <MDBCardBody>
+                      <MDBRow>
+                        <MDBCol md="2">
+                          <MDBCardImage
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/13.webp"
+                            fluid
+                            alt="Phone"
+                          />
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0">Samsung Galaxy</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">White</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">
+                            Capacity: 64GB
+                          </p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">Qty: 1</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">$499</p>
+                        </MDBCol>
+                      </MDBRow>
+                      <hr
+                        className="mb-4"
+                        style={{ backgroundColor: "#e0e0e0", opacity: 1 }}
+                      />
+                      <MDBRow className="align-items-center">
+                        <MDBCol md="2">
+                          <p className="text-muted mb-0 small">Track Order</p>
+                        </MDBCol>
+                        <MDBCol md="10">
+                          <MDBProgress
+                            style={{ height: "6px", borderRadius: "16px" }}
+                          >
+                            <MDBProgressBar
+                              style={{
+                                borderRadius: "16px",
+                                backgroundColor: "#a8729a",
+                              }}
+                              width={65}
+                              valuemin={0}
+                              valuemax={100}
+                            />
+                          </MDBProgress>
+                          <div className="d-flex justify-content-around mb-1">
+                            <p className="text-muted mt-1 mb-0 small ms-xl-5">
+                              Out for delivary
+                            </p>
+                            <p className="text-muted mt-1 mb-0 small ms-xl-5">
+                              Delivered
+                            </p>
+                          </div>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardBody>
+                  </MDBCard>
+
+                  <MDBCard className="shadow-0 border mb-4">
+                    <MDBCardBody>
+                      <MDBRow>
+                        <MDBCol md="2">
+                          <MDBCardImage
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/1.webp"
+                            fluid
+                            alt="Phone"
+                          />
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0">iPad</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">Pink rose</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">
+                            Capacity: 32GB
+                          </p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">Qty: 1</p>
+                        </MDBCol>
+                        <MDBCol
+                          md="2"
+                          className="text-center d-flex justify-content-center align-items-center"
+                        >
+                          <p className="text-muted mb-0 small">$399</p>
+                        </MDBCol>
+                      </MDBRow>
+                      <hr
+                        className="mb-4"
+                        style={{ backgroundColor: "#e0e0e0", opacity: 1 }}
+                      />
+                      <MDBRow className="align-items-center">
+                        <MDBCol md="2">
+                          <p className="text-muted mb-0 small">Track Order</p>
+                        </MDBCol>
+                        <MDBCol md="10">
+                          <MDBProgress
+                            style={{ height: "6px", borderRadius: "16px" }}
+                          >
+                            <MDBProgressBar
+                              style={{
+                                borderRadius: "16px",
+                                backgroundColor: "#a8729a",
+                              }}
+                              width={20}
+                              valuemin={0}
+                              valuemax={100}
+                            />
+                          </MDBProgress>
+                          <div className="d-flex justify-content-around mb-1">
+                            <p className="text-muted mt-1 mb-0 small ms-xl-5">
+                              Out for delivary
+                            </p>
+                            <p className="text-muted mt-1 mb-0 small ms-xl-5">
+                              Delivered
+                            </p>
+                          </div>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardBody>
+                  </MDBCard>
+
+                  <div className="d-flex justify-content-between pt-2">
+                    <p className="fw-bold mb-0">Order Details</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Total</span> $898.00
+                    </p>
+                  </div>
+
+                  <div className="d-flex justify-content-between pt-2">
+                    <p className="text-muted mb-0">Invoice Number : 788152</p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Discount</span> $19.00
+                    </p>
+                  </div>
+
+                  <div className="d-flex justify-content-between">
+                    <p className="text-muted mb-0">
+                      Invoice Date : 22 Dec,2019
+                    </p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">GST 18%</span> 123
+                    </p>
+                  </div>
+
+                  <div className="d-flex justify-content-between mb-5">
+                    <p className="text-muted mb-0">
+                      Recepits Voucher : 18KU-62IIK
+                    </p>
+                    <p className="text-muted mb-0">
+                      <span className="fw-bold me-4">Delivery Charges</span>{" "}
+                      Free
+                    </p>
+                  </div>
+                </MDBCardBody>
+                <MDBCardFooter
+                  className="border-0 px-4 py-5"
+                  style={{
+                    backgroundColor: "#a8729a",
+                    borderBottomLeftRadius: "10px",
+                    borderBottomRightRadius: "10px",
+                  }}
+                >
+                  <MDBTypography
+                    tag="h5"
+                    className="d-flex align-items-center justify-content-end text-white text-uppercase mb-0"
+                  >
+                    Total paid: <span className="h2 mb-0 ms-2">$1040</span>
+                  </MDBTypography>
+                </MDBCardFooter>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
     
-    {user&&user.cart.map((x)=>(
+    {/* {user&&user.cart.map((x)=>(
       <div>
          <MDBCard style={{width:'300px',marginTop:'40px',marginLeft:'400px'}}>
       <MDBCardImage src={x.image} position='top' alt='...' />
@@ -62,14 +299,14 @@ console.log(user.cart.length);
     {x.qty * x.price}
 
         </div>
-    ))}
+    ))} */}
 
   
 
-    Total:{user&&user.cart.reduce((acc,curr)=>acc += curr.qty*curr.price,0)}
+    {/* Total:{user&&user.cart.reduce((acc,curr)=>acc += curr.qty*curr.price,0)} */}
     
     </>
-      </div>
+
   )
   
 }
